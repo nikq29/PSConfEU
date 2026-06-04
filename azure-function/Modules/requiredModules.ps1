f887227bccd59b062184130e4b2145891ff0727c
Niko P <118130328+nikq29@users.noreply.github.com>
2026-06-04 07:03:44 +0200

    update modules

--- azure-function/Modules/requiredModules.ps1
+++ azure-function/Modules/requiredModules.ps1
@@ -1,9 +1,9 @@
 
 # List the modules that will be embedded into Azure Function App deployment
 $requiredModules = @{
-   "Microsoft.Graph.Authentication" = "2.37.0"
-   "Microsoft.Graph.Users"          = "2.37.0"
-   "Microsoft.Graph.Groups"         = "2.37.0"
+   "Microsoft.Graph.Authentication" = "2.36.0"
+   "Microsoft.Graph.Users"          = "2.36.0"
+   "Microsoft.Graph.Groups"         = "2.36.0"
    "Az.Accounts"                    = "4.0.0"
 }
 
